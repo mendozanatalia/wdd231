4b486296c64a33b6a2c03259e054a5829c98d306
mendozanatalia <nmendoza@byupathway.edu>
2025-06-08 22:48:41 -0400

    modals files created and updated

--- scripts/modals.js
+++ scripts/modals.js
@@ -1,21 +1,26 @@
-const dialog = document.querySelector('#course-details');
-const openBtn = document.querySelector('#open-btn');
-const closeBtn = document.querySelector('#close-btn');
+import { courses } from ".scripts/courses-data.mjs";
+// console.log(courses);
 
-function displayCourseDetails(course) {
-    details.innerHTML = '';
-    details.innerHTML = `
-    <button id="closeModal">❌</button>
-    <h2>${course.subject} ${course.number}</h2>
-    <h3>${course.title}</h3>
-    <p><strong>Credits: </strong>${course.credits}</p>
-    <p><strong>Certificate: </strong>${course.certificate}</p>
-    <p>${course.description}</p>
-    <p><strong>Technologies: </strong>${course.technology}</p>
-    `;
-    details.showModal();
-    
-    closeModal.addEventListener("click", () => {
-        details.close();
-    });
+const showHere = document.querySelector('#show-here');
+const dialog = document.querySelector('#course-details');
+const title = document.querySelector('#course-details h2');
+const closeBtn = document.querySelector('#course-details button');
+const info = document.querySelector('#course-details p');
+
+closeBtn.addEventListener("click", () => dialog.close())
+
+function displayCourseDetails(data) {
+    console.log(data)
 }
+
+displayCourseDetails(courses);
+
+    // details.innerHTML = `
+    // <button id="closeBtn">❌</button>
+    // <h2>${course.subject} ${course.number}</h2>
+    // <h3>${course.title}</h3>
+    // <p><strong>Credits: </strong>${course.credits}</p>
+    // <p><strong>Certificate: </strong>${course.certificate}</p>
+    // <p>${course.description}</p>
+    // <p><strong>Technologies: </strong>${course.technology}</p>
+    // `;
